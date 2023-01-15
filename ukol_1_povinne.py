22e915e9a803aa489514dfa04d97ddf9980a9b1d
Iva Simicova <iva_simicova@hotmail.com>
2023-01-15 17:15:35 +0100

    Ukol 1 povinne

---
+++ ukol_1_povinne.py
@@ -0,0 +1,13 @@
+""" Zadání:
+Napiš program, který bude obsahovat jednu proměnnou jmeno. Tato proměnná bude obsahovat libovolné křestní jméno (třeba tvoje).
+Tvůj program vytvoří e-mailovou adresu na základě této proměnné, s doménou czechitas.cz a tuto e-mailovou adresu vypíše.
+Tedy pokud bude hodnota proměnné jmeno například Jindřiška, pak program vypíše Jindřiška@czechitas.cz """
+#Řešení:
+#varianta 1:
+""" jmeno = "Iva"
+print(f"{jmeno}@czechitas.cz") """
+#varianta 2:
+print("Ahoj nastavíme spolu Tvoji email adresu v Czechitas.")
+jmeno = input("Zadej prosím své křestní jméno")
+print(f"{jmeno}@czechitas.cz")
+
